6de5b3e53af49e3d6692bcfcea4e7b608bdf83d6
Siri-Chityala <245122748026@mvsrec.edu.in>
2025-03-18 12:55:04 +0530

    Update Demo1.py

--- Demo1.py
+++ Demo1.py
@@ -1,7 +1,4 @@
 
+num = 5  
 
-num = int(input("Enter a number: "))
-if (num % 2) == 0:
-   print("{0} is Even".format(num))
-else:
-   print("{0} is Odd".format(num))
+print(f"The entered number is: {num}")
